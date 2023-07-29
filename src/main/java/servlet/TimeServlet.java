bac9a5821a10595835968a43eb6181753f7c0ef4
Nikola2112 <nikola21122004@gmail.com>
2023-07-29 17:07:14 +0100

    Unit9Dev

--- src/main/java/servlet/TimeServlet.java
+++ src/main/java/servlet/TimeServlet.java
@@ -1,44 +1,65 @@
 package servlet;
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
-import javax.servlet.ServletException;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
+
+    private TemplateEngine engine;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+
+        engine = new TemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=utf-8");
 
-        String timezoneParam = request.getParameter("timezone");
-        ZoneId zoneId = ZoneId.of("UTC");
 
-        if (timezoneParam != null && !timezoneParam.isEmpty()) {
-            try {
-                zoneId = ZoneId.of(timezoneParam);
-            } catch (Exception e) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid timezone");
-                return;
-            }
+        String timezoneParam = (String) request.getAttribute("timezone");
+        if (timezoneParam == null) {
+            timezoneParam = "UTC";
         }
 
+        ZoneId zoneId = ZoneId.of(timezoneParam);
         LocalDateTime currentTime = LocalDateTime.now(zoneId);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'");
         String formattedTime = currentTime.format(formatter);
 
-        response.getWriter().write("<html><body><h1>Current Time</h1><p>" + formattedTime + "</p>");
+        Context context = new Context();
+        context.setVariable("formattedTime", formattedTime);
+
+        PrintWriter writer = response.getWriter();
+        engine.process("time", context, writer);
+        writer.flush();
     }
 }
+
 
 //надо написть регион/город например: time?timezone=America/Denver , time?timezone=Europe/Kiev
 /*@Override
--- src/main/java/servlet/TimezoneValidateFilter.java
+++ src/main/java/servlet/TimezoneValidateFilter.java
@@ -2,12 +2,19 @@ package servlet;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 @WebFilter("/time")
 public class TimezoneValidateFilter implements Filter {
+
+    private static final String COOKIE_NAME = "lastTimezone";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -18,10 +25,40 @@ public class TimezoneValidateFilter implements Filter {
                 ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid timezone");
                 return;
             }
+
+
+            timezoneParam = URLEncoder.encode(timezoneParam, StandardCharsets.UTF_8.toString());
+            Cookie cookie = new Cookie(COOKIE_NAME, timezoneParam);
+            cookie.setMaxAge(60 * 60 * 24); // 1 день
+            ((HttpServletResponse) response).addCookie(cookie);
+        } else {
+
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            Cookie[] cookies = httpRequest.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals(COOKIE_NAME)) {
+                        String savedTimezone = cookie.getValue();
+                        if (isValidTimezone(savedTimezone)) {
+                            timezoneParam = savedTimezone;
+                            break;
+                        }
+                    }
+                }
+            }
         }
+
+
+        if (timezoneParam == null) {
+            timezoneParam = "UTC";
+        }
+
+
+        request.setAttribute("timezone", timezoneParam);
 
         chain.doFilter(request, response);
     }
+
     private boolean isValidTimezone(String timezone) {
         try {
             TimeZone.getTimeZone(timezone);
